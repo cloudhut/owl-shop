8f37ba56df8762f3b8402050dc9f1aea62fd44a1
Martin Schneppenheim <23424570+weeco@users.noreply.github.com>
2023-07-27 23:43:11 +0100

    move all proto schemas into proto dir

--- proto/embed.go
+++ proto/embed.go
@@ -5,8 +5,10 @@ import _ "embed"
 var (
 	//go:embed shop/v1/address.proto
 	Address string
-	//go:embed shop/v1/customer.proto
-	Customer string
+	//go:embed shop/v1/customer_v1.proto
+	CustomerV1 string
+	//go:embed shop/v1/customer_v2.proto
+	CustomerV2 string
 	//go:embed shop/v1/order.proto
 	Order string
 )
